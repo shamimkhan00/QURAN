db94b69d997f3aa088d2278d6438618f3478ba0f
Shamim Khan <157951605+shamimkhan00@users.noreply.github.com>
2025-06-11 17:59:13 +0530

    pnpm

--- src/firebase.js
+++ src/firebase.js
@@ -8,11 +8,11 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: "quran-c4bc3",
-  storageBucket: "quran-c4bc3.appspot.com",  // Fixed `.app` to `.appspot.com  storageBucket: "quran-c4bc3.appspot.com"`
-  messagingSenderId: "550017202288",
-  appId: "1:550017202288:web:c5bc163418cccf2e9d1cb5",
-  measurementId: "G-VGHEVW8VK6"
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
